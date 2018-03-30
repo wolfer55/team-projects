fe70b4309fd2d9d41f6232b673f5d74b98199751
wolfer55 <wolfer.4@wright.edu>
2018-03-30 11:21:24 -0700

    Added the correct play pause functionality

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
 import pyaudio
 import wave
-
-
+import vlc
+from pygame import mixer
+CHUNK = 256
 
 
 class Application:
 	def __init__(self,master):
 		self.builder = builder = pygubu.Builder()
-
+		
+		mixer.init()
 
 		builder.add_from_file('teamProj.ui')
 
@@ -19,21 +21,39 @@ class Application:
 
 		builder.connect_callbacks(self) # Creates the call backs for the gui operations
 
-		self.chunk = 1024
+
+		self.should_play=True
+
+		self.pa = pyaudio.PyAudio()
+
+
+
+		
+
+
+
+#	def playingCallback(in_data, frame_count, time_info, status):
+#		data = self.file_imported.readframes(frame_count)
+#		return (data, pyaudio.paContinue)
+
+		self.play_pause=False
+		self.started=False
 
 	def on_play_button_click(self):
-		self.aud = pyaudio.PyAudio()
-		if self.canRun:
-			self.stream = self.aud.open(format = self.aud.get_format_from_width(self.file_imported.getsampwidth()),channels = self.file_imported.getnchannels(),rate = self.file_imported.getframerate(), output = True)
+		if not self.started:
+			mixer.music.play()
+			self.started = True
+		else:	
+			if self.play_pause:
+				mixer.music.unpause()
+				self.play_pause=False
+			else:
+				mixer.music.pause()
+				self.play_pause=True
 
-			self.data = self.file_imported.readframes(self.chunk)
-
-			while self.data:
-				self.stream.write(self.data)
-				self.data = self.file_imported.readframes(self.chunk)
 
 	def on_ff_button_click(self):
-		print("hello this wokrs")
+		print("hello")
 
 	def on_import_click(self):
 		self.fileName = askopenfilename(filetypes=[("MP3 files" ,"*.mp3"),("Wave files" , "*.wav")])
@@ -49,7 +69,8 @@ class Application:
 		print("hello this wokrs")
 
 	def on_rewind_button_click(self):
-		print("hello this wokrs")
+		mixer.music.rewind()
+
 
 	def on_process_click(self):
 		print("yep")
@@ -62,13 +83,13 @@ class Application:
 	def loadFile(self):
 
 		if self.fileName:
-			try:
-				print(self.fileName)
-				self.canRun = True
-				self.file_imported = wave.open(self.fileName,"rb")
-			except:
-				showerror("Cant open this type of File")
-
+			#try:
+			print(self.fileName)
+			self.canRun = True
+			#self.file_imported = vlc.MediaPlayer(self.fileName)
+			mixer.music.load(self.fileName)
+			#except:
+				#showerror("Cant open this type of File")
 
 if __name__ == '__main__':
 	root = tk.Tk()
